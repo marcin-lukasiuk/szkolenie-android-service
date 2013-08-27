9001cfff11a101286e283110f0cfce0ee8e4a37f
Rafał Piotrowski <rafal.piotrowski@unity.pl>
2013-08-27 16:41:35 +0200

    pendint intent

--- src/com/example/szkolenie_hello_service/HelloReceiver.java
+++ src/com/example/szkolenie_hello_service/HelloReceiver.java
@@ -2,25 +2,34 @@ package com.example.szkolenie_hello_service;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.widget.Toast;
 
 public class HelloReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
-
-		Notification notification = new NotificationCompat.Builder(context)
-		.setSmallIcon(R.drawable.ic_launcher)
-				.setContentTitle("Hi!").setContentText("Content")
-				.setTicker("Hi Ticker").build();
+		PendingIntent pendingIntent = PendingIntent.getActivity(
+				context,
+				0,
+				new Intent(context, MainActivity.class),
+				PendingIntent.FLAG_UPDATE_CURRENT);
 		
-		NotificationManager notificationManager = (NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notification = new NotificationCompat.Builder(context)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Hi!")
+				.setContentText("Content")
+				.setTicker("Hi Ticker")
+				.setContentIntent(pendingIntent)
+				.setAutoCancel(true)
+				.build();
+		
+		NotificationManager notificationManager =
+				(NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		
 		notificationManager.notify(0, notification);
 	}
