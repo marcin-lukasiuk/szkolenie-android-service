80b013d1eeb56ab223163821e3964ef9673dc4f3
mlukasiu <mlukasiu@euroteam.(none)>
2013-08-27 16:34:35 +0200

    notyfikacja

--- src/com/example/szkolenie_hello_service/HelloReceiver.java
+++ src/com/example/szkolenie_hello_service/HelloReceiver.java
@@ -1,17 +1,28 @@
 package com.example.szkolenie_hello_service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class HelloReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(context, "Service sais hello BR!",
-				Toast.LENGTH_SHORT).show();
 
+
+		Notification notification = new NotificationCompat.Builder(context)
+		.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Hi!").setContentText("Content")
+				.setTicker("Hi Ticker").build();
+		
+		NotificationManager notificationManager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		notificationManager.notify(0, notification);
 	}
 
 }
